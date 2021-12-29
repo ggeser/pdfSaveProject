2429fbcf4b07aed84ce8844e71db937efcb22c3f
sergeinikolaev <ggeser@gmail.com>
2021-12-29 23:15:43 +0300

    com1

--- pdf-save/app/Http/Controllers/Controller.php
+++ pdf-save/app/Http/Controllers/Controller.php
@@ -6,8 +6,27 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Обновить аватар пользователя.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function upload(Request $request)
+    {
+//        $path = $request->file('avatar')->store('avatars');
+//        $path = Storage::putFile('avatars', $request->file('avatar'));
+
+
+        $path = Storage::disk('s3')->put('foo/bar/baz.txt', 'some content', 'public');
+
+        return $path;
+    }
 }
